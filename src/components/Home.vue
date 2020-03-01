970194b763c22f761cc06ad4dab2bde975deb328
zhuyicheng666 <974246866@qq.com>
2020-03-01 16:11:56 +0800

    完成用户列表功能的开发

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,125 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+  <el-container class="home-container">
+    <!-- 头部区域 -->
+    <el-header>
+      <div>
+        <img src="../assets/gouwu.png" alt="">
+        <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+    </el-header>
+    <!--页面主体区-->
+    <el-container>
+      <!--页面侧边栏-->
+      <el-aside :width="isCollapse?'64px':'200px'">
+        <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <el-menu
+          background-color="#313744"
+          text-color="#fff"
+          active-text-color="#409EFF"
+          :unique-opened="true"
+          :collapse="isCollapse"
+          :collapse-transition="false"
+          :router="true"
+          :default-active="this.$route.path"
+          >
+          <!--一级菜单-->
+          <el-submenu :index="item.id+''" v-for="item in menuList" :key="item.id">
+            <!--一级菜单模板区域-->
+            <template slot="title">
+              <!--图标-->
+              <i class="el-icon-location"></i>
+              <!--文本-->
+              <span>{{item.authName}}</span>
+            </template>
+            <!--二级菜单-->
+            <el-menu-item :index="'/' + subItem.path" v-for="subItem in item.children" :key="subItem.id" >
+              <!--二级菜单模板区域-->
+              <template slot="title">
+                <!--图标-->
+                <i class="el-icon-menu"></i>
+                <!--文本-->
+                <span>{{subItem.authName}}</span>
+              </template>
+            </el-menu-item>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
+      <!--页面内容主体-->
+      <el-main>
+        <!--路由占位符-->
+        <router-view></router-view>
+      </el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      // 左侧菜单数据
+      menuList: [],
+      isCollapse: false
+    }
+  },
+  created () {
+    this.getMenuList()
+  },
   methods: {
     logout () {
       window.sessionStorage.clear()
       this.$router.push('/login')
+    },
+    async getMenuList () {
+      const { data: res } = await this.$http.get('menus')
+      if (res.meta.status !== 200) return this.$message.error(res.meta.msg)
+      this.menuList = res.data
+      console.log(res)
+    },
+    toggleCollapse () {
+      this.isCollapse = !this.isCollapse
     }
   }
 }
 </script>
 
 <style lang="less" scoped>
+.el-header{
+  background-color:rgb(52, 54, 54);
+  display: flex;
+  justify-content: space-between;
+  padding-left: 0;
+  align-items: center;
+  color: #ffffff;
+  font-size: 20px;
+  >div{
+    display: flex;
+    align-items: center;
+    span{
+      margin-left: 15px;
+    }
+  }
+}
+.el-aside{
+  background-color:rgb(49, 55, 68);
+   .el-menu{
+    border-right:none
+  }
+ }
+.el-main{
+  background-color: rgb(232, 236, 236)
+}
+.home-container{
+  height: 100%
+}
+.toggle-button{
+  background-color:#4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color:#ffffff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,28 @@
 import Vue from 'vue'
-import { Button, Form, Input, FormItem, Message } from 'element-ui'
+import { Button, MessageBox, Dialog, Pagination, Table, Tooltip, Switch, TableColumn, Row, Col, Form, Input, FormItem, Message, Container, Header, Aside, Main, Menu, Submenu, MenuItemGroup, MenuItem, Breadcrumb, BreadcrumbItem, Card } from 'element-ui'
 
 Vue.use(Button)
 Vue.use(Form)
 Vue.use(FormItem)
 Vue.use(Input)
+Vue.use(Container)
+Vue.use(Header)
+Vue.use(Aside)
+Vue.use(Main)
+Vue.use(Menu)
+Vue.use(Submenu)
+Vue.use(MenuItem)
+Vue.use(MenuItemGroup)
+Vue.use(Breadcrumb)
+Vue.use(BreadcrumbItem)
+Vue.use(Card)
+Vue.use(Row)
+Vue.use(Col)
+Vue.use(Table)
+Vue.use(TableColumn)
+Vue.use(Switch)
+Vue.use(Tooltip)
+Vue.use(Pagination)
+Vue.use(Dialog)
 Vue.prototype.$message = Message
+Vue.prototype.$confirm = MessageBox.confirm
